2c09ac3ff0e1a6e8b123d3b40bfd164e071de136
vinithecodifier <vinicius.lima58@aluno.cps.sp.gov.br>
2026-09-15 21:20:30 -0300

    inicio da estrutura

---
+++ pedidosja.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="mb-0 mt-2 ">Bistrot Chez Rémy</h1>
+        <p class = "mt-0">Cardápio de mesa</p>
+        <div>
+            <label for="mesa">Mesa:</label>
+            <input type="number" classn = "form-control" id="mesa" min = 1 placeholder="N°" class="mb-3" >
+        </div>
+        <div>
+
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Entradas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Bebidas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Lanches</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Salgados</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        
+    </main>
+</body>
+
+</html>
